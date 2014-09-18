c87201ef04bdac722c020dd0c26d81de0a965053
Craig Douglas <craig@eldougo.net>
2014-09-18 10:32:50 +1000

    reformatted using tabs as per Wordpress conventions

--- tuxquote.php
+++ tuxquote.php
@@ -59,32 +59,29 @@ function tuxquote_choose_image() {
  * Build and format HTML output.
  */
  function tuxquote_build_format( $div_width, $title = '' ) {
-    if ( empty( $div_width ) ) {
-        $div_width = TUXQUOTE_DEFAULT_WIDTH;
-    }
-    if ( is_numeric( $div_width ) ) {
-        $div_width = trim( $div_width ) . "%";
-    }
-    if ( ! empty( $title ) ) {
-         $title_line = "  <p style='text-align: center; font-weight: 900'>".$title."</p>\n";
-    } else {
-        $title_line = '';
-    }
-    return  "\n<div style='float: right; width: ".$div_width."; '>\n"
-            .$title_line
-            ."  <img src='".tuxquote_choose_image()."'><br />\n"
-            ."  <p style='text-align: center'>".tuxquote_choose_quote()."</p>\n"
-            ."</div>\n";
+	if ( empty( $div_width ) ) {
+		$div_width = TUXQUOTE_DEFAULT_WIDTH;
+	}
+	if ( is_numeric( $div_width ) ) {
+		$div_width = trim( $div_width ) . "%";
+	}
+	if ( ! empty( $title ) ) {
+		 $title_line = "  <p style='text-align: center; font-weight: 900'>".$title."</p>\n";
+	} else {
+		$title_line = '';
+	}
+	return  "\n<div style='float: right; width: ".$div_width."; '>\n"
+			.$title_line
+			."  <img style='width:100%' src='".tuxquote_choose_image()."'><br />\n"
+			."  <p style='text-align: center'>".tuxquote_choose_quote()."</p>\n"
+			."</div>\n";
  }
 
 /**
  * Return HTML encoded random image and quote.
  */
 function tuxquote_main( $atts ) {
-    $a = shortcode_atts( array(
-            'width' => TUXQUOTE_DEFAULT_WIDTH,
-            'title' => ''
-            ), $atts );
+	$a = shortcode_atts( array( 'width' => TUXQUOTE_DEFAULT_WIDTH, 'title' => ''  ), $atts );
 	return  tuxquote_build_format( $a['width'],  $a['title'] );
 }
 
@@ -93,95 +90,94 @@ function tuxquote_main( $atts ) {
  */
 class Tuxquote_Widget extends WP_Widget {
 
-    const DEFAULT_WIDTH = '100%'; // Default div with for widget print.
+	const DEFAULT_WIDTH = '100%'; // Default div with for widget print.
 
-    /**
-     * Register widget with WordPress.
-     */
-    function __construct() {
-        parent::__construct(
-            'tuxquote_widget', // Base ID
-            __('Tuxquote', 'tuxquote_domain'), // Name
-            array( 'description' => __( 'Show a random image of Tux the Linux penguin with a random quote.', 'tuxquote_domain' ), ) // Args
-        );
-    }
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'tuxquote_widget', // Base ID
+			__('Tuxquote', 'tuxquote_domain'), // Name
+			array( 'description' => __( 'Show a random image of Tux the Linux penguin with a random quote.','tuxquote_domain' ), ) // Args
+		);
+	}
 
-    /**
-     * Front-end display of widget.
-     *
-     * @see WP_Widget::widget()
-     *
-     * @param array $args     Widget arguments.
-     * @param array $instance Saved values from database.
-     */
-    public function widget( $args, $instance ) {
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args	 Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
 
-        echo $args['before_widget'];
-        if ( ! empty( $instance['title'] ) ) {
-            echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
-        }
-        if ( empty( $instance['width'] ) ) {
-            $instance['width'] = self::DEFAULT_WIDTH;
-        }
-        echo tuxquote_build_format( $instance['width'] );
-        echo $args['after_widget'];
-    }
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}
+		if ( empty( $instance['width'] ) ) {
+			$instance['width'] = self::DEFAULT_WIDTH;
+		}
+		echo tuxquote_build_format( $instance['width'] );
+		echo $args['after_widget'];
+	}
 
-    /**
-     * Back-end widget form.
-     *
-     * @see WP_Widget::form()
-     *
-     * @param array $instance Previously saved values from database.
-     */
-    public function form( $instance ) {
-        if ( isset( $instance[ 'title' ] ) ) {
-            $title = $instance[ 'title' ];
-        }
-        else {
-            $title = '';
-        }
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = '';
+		}
 
-        if ( isset( $instance[ 'width' ] ) ) {
-            $width = $instance[ 'width' ];
-        }
-        else {
-            $width = self::DEFAULT_WIDTH;
-        }
+		if ( isset( $instance[ 'width' ] ) ) {
+			$width = $instance[ 'width' ];
+		}
+		else {
+			$width = self::DEFAULT_WIDTH;
+		}
 
-        ?>
-        <p>
-        <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
-        <label for="<?php echo $this->get_field_id( 'width' ); ?>"><?php _e( 'Width:' ); ?></label>
-        <input class="widefat" id="<?php echo $this->get_field_id( 'width' ); ?>" name="<?php echo $this->get_field_name( 'width' ); ?>" type="text" value="<?php echo esc_attr( $width ); ?>">
-        </p>
-        <?php
-    }
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		<label for="<?php echo $this->get_field_id( 'width' ); ?>"><?php _e( 'Width:' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'width' ); ?>" name="<?php echo $this->get_field_name( 'width' ); ?>" type="text" value="<?php echo esc_attr( $width ); ?>">
+		</p>
+		<?php
+	}
 
-    /**
-     * Sanitize widget form values as they are saved.
-     *
-     * @see WP_Widget::update()
-     *
-     * @param array $new_instance Values just sent to be saved.
-     * @param array $old_instance Previously saved values from database.
-     *
-     * @return array Updated safe values to be saved.
-     */
-    public function update( $new_instance, $old_instance ) {
-        $instance = array();
-        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-        $instance['width'] = ( ! empty( $new_instance['width'] ) ) ? strip_tags( $new_instance['width'] ) : self::DEFAULT_WIDTH;
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['width'] = ( ! empty( $new_instance['width'] ) ) ? strip_tags( $new_instance['width'] ) : self::DEFAULT_WIDTH;
 
-        return $instance;
-    }
+		return $instance;
+	}
 
 } // class Tuxquote_Widget
 
 // register Tuxquote_Widget widget
 function register_tuxquote_widget() {
-    register_widget( 'Tuxquote_Widget' );
+	register_widget( 'Tuxquote_Widget' );
 }
 add_action( 'widgets_init', 'register_tuxquote_widget' );
-
